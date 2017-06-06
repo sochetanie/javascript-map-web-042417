11a6a5df6638766598d0bc192b8f4b23b1f94476
sochetanie <allagorik2901@gmail.com>
2017-06-06 17:34:07 -0400

    done

---
+++ index.js
@@ -0,0 +1,27 @@
+function map (collection, callback) {
+    const result = []
+    for (let i =0; i < collection.length; i++) {
+        const element = collection[i]
+        result.push(callback(element, i, collection))
+    }
+    return result
+}
+
+
+
+
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+ 
+const transformedAutobots = map(autobots, function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+ 
+console.log(transformedAutobots);
