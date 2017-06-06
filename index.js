0e4278c9ecbbfabbe4b72cb0174097fc4c50622b
sochetanie <allagorik2901@gmail.com>
2017-06-06 17:36:05 -0400

    done

--- index.js
+++ index.js
@@ -25,3 +25,14 @@ const transformedAutobots = map(autobots, function (autobot) {
 });
  
 console.log(transformedAutobots);
+
+// =>>>>>>
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
